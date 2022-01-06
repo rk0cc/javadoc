c3eac59b504a005951cf4823b1713a82113ca11d
Cyrus Chan <cyruschan1212@gmail.com>
2022-01-06 16:42:53 +0800

    Push Javadoc

---
+++ docs/fetch_versions.js
@@ -0,0 +1,35 @@
+"use strict";
+((document) => {
+    async function loadJDocDir() {
+        return await fetch("https://osp.rk0cc.xyz/javadoc/dir.min.json", {
+            method: "GET",
+            redirect: "follow"
+        }).then(resp => resp.json());
+    }
+
+    const jdgp = document.head.querySelector("meta[name=javagroup]").getAttribute("content");
+    const jdai = document.head.querySelector("meta[name=javaartifact]").getAttribute("content");
+
+    var headerBody = document.createElement("h1");
+    headerBody.appendChild(document.createTextNode("Package: xyz.rk0cc." + jdgp + ":" + jdai));
+    document.body.appendChild(headerBody);
+
+    var vul = document.createElement("ul");
+    document.body.appendChild(vul);
+
+    loadJDocDir()
+        .then((jdoc) => {
+            var targetNode = jdoc.filter(j => j.group === jdgp && j.artifact == jdai);
+
+            if (targetNode.length !== 1) throw new Error("Duplicated package found");
+
+            for (const ver of targetNode[0].versions.released) {
+                var l = document.createElement("a");
+                l.href = "./" + ver + "/";
+                l.appendChild(document.createTextNode(ver));
+                var li = document.createElement("li");
+                li.appendChild(l);
+                vul.appendChild(li);
+            }
+        });
+})(document);
